db51a4051a6df591bdc7ca426c512d63ce9dca37
naveen <naveen.panwar27@gmail.com>
2017-01-29 19:19:18 +0530

    usen some tags to remove static content to show the wp content the_post the_title the_date the_author

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 <div class="row">
 
   <div class="col-sm-8 blog-main">
-	
-    <?php get_template_part('content', get_post_format()); ?>
+
+    <?php
+        if ( have_posts() ): while ( have_posts() ): the_post();
+            get_template_part('content', get_post_format());
+        endwhile; endif;
+    ?>
 
     <nav>
       <ul class="pager">
